a517c568b8629d2bf08381081464d2ae66df98a6
Robert Teschmacher <r.teschmacher@gmail.com>
2024-12-31 13:24:44 +0000

    deps update + rebuild

--- src/components/QuizScreen.vue
+++ src/components/QuizScreen.vue
@@ -129,7 +129,7 @@ export default defineComponent({
       emit("submit-answer");
     };
 
-    const emitUpdateAnswer = (answer: string) => {
+    const emitUpdateAnswer = (answer: string | number) => {
       emit("update:userAnswer", answer);
     };
 
